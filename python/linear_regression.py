1b0bf11e26c692b91ba61a78d69b05be6d055255
Arnav <ArnavVerma@my.unt.edu>
2025-07-27 17:27:24 -0500

    linear regression

--- python/linear_regression.py
+++ python/linear_regression.py
@@ -1,79 +1,23 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.axes as ax
-from matplotlib.animation import FuncAnimation
 
 
-
-url = ""
-data = pd.read_csv(url)
-
-data = data.dropna()
-
-train_input = np.array(data.x[0:500]).reshape(500, 1)
-train_output = np.array(data.y[0:500]).reshape(500, 1)
-
-test_input = np.array(data.x[500:700]).reshape(199, 1)
-test_output = np.array(data.y[500:700]).reshape(199, 1)
 
 class LinearRegression: 
-    def __init__(self): 
-        self.parameters = {} 
+    
+    def __init__(self, trainInput ) -> None:
+        self.trainInput = trainInput  
+        
+    def forward(self):
+        pass
+    
+    
+    def backprop(self):
+        pass
+    
+    def update(self):
+        pass
 
-    def forward_propagation(self, train_input): 
-        m = self.parameters['m'] 
-        c = self.parameters['c'] 
-        predictions = np.multiply(m, train_input) + c 
-        return predictions 
-
-    def cost_function(self, predictions, train_output): 
-        cost = np.mean((train_output - predictions) ** 2) 
-        return cost 
-
-    def backward_propagation(self, train_input, train_output, predictions): 
-        derivatives = {} 
-        df = (predictions-train_output) 
-        dm = 2 * np.mean(np.multiply(train_input, df)) 
-        dc = 2 * np.mean(df) 
-        derivatives['dm'] = dm 
-        derivatives['dc'] = dc 
-        return derivatives 
-
-    def update_parameters(self, derivatives, learning_rate): 
-        self.parameters['m'] = self.parameters['m'] - learning_rate * derivatives['dm'] 
-        self.parameters['c'] = self.parameters['c'] - learning_rate * derivatives['dc'] 
-
-    def train(self, train_input, train_output, learning_rate, iters): 
-        self.parameters['m'] = np.random.uniform(0, 1) * -1
-        self.parameters['c'] = np.random.uniform(0, 1) * -1
-
-        self.loss = [] 
-
-        fig, ax = plt.subplots() 
-        x_vals = np.linspace(min(train_input), max(train_input), 100) 
-        line, = ax.plot(x_vals, self.parameters['m'] * x_vals + self.parameters['c'], color='red', label='Regression Line') 
-        ax.scatter(train_input, train_output, marker='o', color='green', label='Training Data') 
-
-        ax.set_ylim(0, max(train_output) + 1) 
-
-        def update(frame): 
-            predictions = self.forward_propagation(train_input) 
-            cost = self.cost_function(predictions, train_output) 
-            derivatives = self.backward_propagation(train_input, train_output, predictions) 
-            self.update_parameters(derivatives, learning_rate) 
-            line.set_ydata(self.parameters['m'] * x_vals + self.parameters['c']) 
-            self.loss.append(cost) 
-            print("Iteration = {}, Loss = {}".format(frame + 1, cost)) 
-            return line, 
-
-        ani = FuncAnimation(fig, update, frames=iters, interval=200, blit=True) 
-        ani.save('linear_regression_A.gif', writer='ffmpeg') 
-
-        plt.xlabel('Input') 
-        plt.ylabel('Output') 
-        plt.title('Linear Regression') 
-        plt.legend() 
-        plt.show() 
-
-        return self.parameters, self.loss
+    def cost_function(self):
+        pass
+    
