bb5baa7371a0bf383f5d3631473f348e4e69c5a8
Arnav <ArnavVerma@my.unt.edu>
2025-07-28 22:21:26 -0500

    svm

--- python/linear_regression.py
+++ python/linear_regression.py
@@ -1,23 +1,8 @@
-import pandas as pd
-import numpy as np
-
-
 
 class LinearRegression: 
     
-    def __init__(self, trainInput ) -> None:
-        self.trainInput = trainInput  
+    def __init__(self, ) -> None:
+        self.params = {}
+    
+    def forward(self, train_inputs):
         
-    def forward(self):
-        pass
-    
-    
-    def backprop(self):
-        pass
-    
-    def update(self):
-        pass
-
-    def cost_function(self):
-        pass
-    
